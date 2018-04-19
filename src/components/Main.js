8e47dbcfa4b81f406181650bb8ab4e6bdf892a2e
yuzexia <327932801@qq.com>
2018-04-19 12:58:29 +0800

    image component

--- src/components/Main.js
+++ src/components/Main.js
@@ -16,14 +16,35 @@ imageDatas = (function genImageURL(imageDatasArr) {
   return imageDatasArr;
 })(imageDatas);
 
+class ImgFigure extends React.Component {
+  render() {
+    return (
+      <figure className="img-figure">
+        <img src={this.props.data.imageURL} alt={this.props.data.title}/>
+        <figcaption>
+          <h2 className="img-title">{this.props.data.title}</h2>
+        </figcaption>
+      </figure>
+    )
+  }
+}
+
 class AppComponent extends React.Component {
   render() {
+    let controllerUnits = [];
+    let imgFigures = [];
+
+    imageDatas.forEach((item) => {
+      imgFigures.push(<ImgFigure data={item}/>)
+    });
+
     return (
       <section className="stage">
         <section className="img-sec">
+          {imgFigures}
         </section>
         <section className="controller-nav">
-
+          {controllerUnits}
         </section>
       </section>
     );
